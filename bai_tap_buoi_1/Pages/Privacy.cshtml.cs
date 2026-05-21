10e914d7224afad1319ecd89d025d493aca22536
cuongdangcap <cuongdcoy@gmail.com>
2026-05-21 08:48:17 +0700

    them bai tap buoi 1

---
+++ bai_tap_buoi_1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace bai_tap_buoi_1.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
